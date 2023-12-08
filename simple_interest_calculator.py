d67d501d08d06cdf1eb1884a46a1c939360a6f38
Wijethunga M.G.C.J <120020452+wijethucs19028@users.noreply.github.com>
2023-12-08 21:18:02 +0530

    Update simple_interest_calculator.py

--- simple_interest_calculator.py
+++ simple_interest_calculator.py
@@ -1,1 +1,11 @@
+def calculate_simple_interest(principal, rate, time):
+    interest = (principal * rate * time) / 100
+    return interest
+
+principal_amount = 1000
+interest_rate = 5
+time_period = 2
+
+result = calculate_simple_interest(principal_amount, interest_rate, time_period)
+print(f"Simple Interest: ${result}")
 
